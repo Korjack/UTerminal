c46a7154977abf1649454cc3e06ee94fc8cef62a
Duhyeon <korjack@duhyeon.kim>
2025-01-31 16:57:23 +0900

    feat: Add log class
    - Add log4net package
        - Add LICENSE for log4net
    - Add a universally available logger
        - For saving serial data
        - For saving system log
    
    Signed-off-by: Duhyeon <korjack@duhyeon.kim>

---
+++ UTerminal/Models/ULogManager.cs
@@ -0,0 +1,76 @@
+﻿using log4net;
+using log4net.Appender;
+using log4net.Core;
+using log4net.Layout;
+using log4net.Repository.Hierarchy;
+
+namespace UTerminal.Models;
+
+public class ULogManager
+{
+    private readonly ILog _log;
+    private RollingFileAppender? _fileAppender;
+
+    public ULogManager(string logType, LogConfig config)
+    {
+        _log = LogManager.GetLogger(logType);
+        ConfigureLogging(config);
+    }
+
+    private void ConfigureLogging(LogConfig config)
+    {
+        Hierarchy hierarchy = (Hierarchy)LogManager.GetRepository();
+
+        _fileAppender = new RollingFileAppender
+        {
+            Name = $"RollingFileAppender-{_log.Logger.Name}",
+            File = config.FilePath,
+            AppendToFile = true,
+            RollingStyle = RollingFileAppender.RollingMode.Date,
+            DatePattern = config.FilePattern,
+            StaticLogFileName = true,
+            MaxSizeRollBackups = config.MaxSizeRollBackups,
+            MaximumFileSize = config.MaximumFileSize,
+            Layout = new PatternLayout
+            {
+                ConversionPattern = config.Layout
+            }
+        };
+
+        ((PatternLayout)_fileAppender.Layout).ActivateOptions();
+        _fileAppender.ActivateOptions();
+
+        Logger logger = hierarchy.GetLogger(_log.Logger.Name) as Logger;
+        logger.AddAppender(_fileAppender);
+        logger.Level = config.LogLevel;
+        logger.Additivity = false;
+
+        hierarchy.Configured = true;
+    }
+
+    public void ChangeLogFilePath(string newLogFilePath)
+    {
+        if (_fileAppender != null)
+        {
+            _fileAppender.File = newLogFilePath;
+            _fileAppender.ActivateOptions();
+        }
+    }
+
+    // 로그 메서드들
+    public void Debug(string message) => _log.Debug(message);
+    public void Info(string message) => _log.Info(message);
+    public void Warn(string message) => _log.Warn(message);
+    public void Error(string message) => _log.Error(message);
+    public void Fatal(string message) => _log.Fatal(message);
+}
+
+public class LogConfig
+{
+    public string FilePath { get; set; } = "./";
+    public string FilePattern { get; set; } = "'.'yyyy-MM-dd";
+    public string Layout { get; set; } = "%date [%thread] %-5level %logger - %message%newline";
+    public int MaxSizeRollBackups { get; set; } = 14;
+    public string MaximumFileSize { get; set; } = "10MB";
+    public Level LogLevel { get; set; } = Level.Debug;
+}
